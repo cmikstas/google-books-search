62160a8b56e58576f09ac192f29340856f98d025
Chris Mikstas <chrismikstas@gmail.com>
2020-01-12 17:25:56 -0700

    created base folder structure.

---
+++ google-search/client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Learn React
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
